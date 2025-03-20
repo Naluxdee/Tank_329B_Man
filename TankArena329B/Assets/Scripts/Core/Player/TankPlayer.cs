04b9d3bfe31d592c387039ccfd6f8acdf22c512e
Naluxdee <arthit.phan@bumail.net>
2025-03-20 23:11:28 +0700

    camera

--- TankArena329B/Assets/Scripts/Core/Player/TankPlayer.cs
+++ TankArena329B/Assets/Scripts/Core/Player/TankPlayer.cs
@@ -2,11 +2,15 @@ using Unity.Netcode;
 using UnityEngine;
 using Unity.Cinemachine;
 using Unity.Collections;
+using System;
 
 public class TankPlayer : NetworkBehaviour
 {
     [Header("References")]
     [SerializeField] private CinemachineCamera virtualCamera;
+
+    [SerializeField] private Texture2D crosshair;
+    [field: SerializeField] public Health Health { get; private set; }
 
     [Header("Settings")]
     [SerializeField] private int ownerPriority = 15;
@@ -14,6 +18,8 @@ public class TankPlayer : NetworkBehaviour
     public NetworkVariable<FixedString32Bytes> PlayerName = new NetworkVariable<FixedString32Bytes>();
     public NetworkVariable<int> PlayerColorIndex = new NetworkVariable<int>();
 
+    public static event Action<TankPlayer> OnPlayerSpawned;
+    public static event Action<TankPlayer> OnPlayerDespawned;
     public override void OnNetworkSpawn()
     {
         if (IsServer)
@@ -23,11 +29,23 @@ public class TankPlayer : NetworkBehaviour
 
             PlayerName.Value = userData.userName;
             PlayerColorIndex.Value = userData.userColorIndex;
+
+            OnPlayerSpawned?.Invoke(this);
         }
 
         if (IsOwner)
         {
             virtualCamera.Priority = ownerPriority;
+
+            Cursor.SetCursor(crosshair, new Vector2(crosshair.width / 2, crosshair.height / 2), CursorMode.Auto);
+        }
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (IsServer)
+        {
+            OnPlayerDespawned?.Invoke(this);
         }
     }
 }
--- TankArena329B/Assets/Scripts/Networking/Server/NetworkServer.cs
+++ TankArena329B/Assets/Scripts/Networking/Server/NetworkServer.cs
@@ -9,13 +9,12 @@ public class NetworkServer : IDisposable
 
     private Dictionary<ulong, string> clientIdToAuth = new Dictionary<ulong, string>();
     private Dictionary<string, UserData> authIdToUserData = new Dictionary<string, UserData>();
-
     public NetworkServer(NetworkManager networkManager)
     {
         this.networkManager = networkManager;
 
         networkManager.ConnectionApprovalCallback += ApprovalCheck;
-        networkManager.OnServerStarted += OnNetworkReady;
+        this.networkManager.OnServerStarted += OnNetworkReady;
     }
 
     private void OnNetworkReady()
@@ -32,9 +31,7 @@ public class NetworkServer : IDisposable
         }
     }
 
-    private void ApprovalCheck(
-        NetworkManager.ConnectionApprovalRequest request,
-        NetworkManager.ConnectionApprovalResponse response)
+    private void ApprovalCheck(NetworkManager.ConnectionApprovalRequest request, NetworkManager.ConnectionApprovalResponse response)
     {
         string payload = System.Text.Encoding.UTF8.GetString(request.Payload);
         UserData userData = JsonUtility.FromJson<UserData>(payload);
@@ -44,9 +41,10 @@ public class NetworkServer : IDisposable
         //Debug.Log(userData.userName);
 
         response.Approved = true;
+        response.Position = SpawnPoint.GetRandomSpawnPos();
+        response.Rotation = Quaternion.identity;
         response.CreatePlayerObject = true;
     }
-
     public UserData GetUserDataByClientId(ulong clientId)
     {
         if (clientIdToAuth.TryGetValue(clientId, out string authId))
